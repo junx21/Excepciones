a7ddb4a0291fe5d71e0fbf6f145f2e9605760514
junx21 <jxuche@myuax.com>
2025-01-06 15:39:18 +0100

    ej2

---
+++ ej2/EJ2.cpp
@@ -0,0 +1,23 @@
+
+#include "EJ2.h"
+#include <STDEXCEPT>
+
+// Versión de la función que devuelve un valor de error
+int divideError(int a, int b) {
+    if (b == 0) {
+        return -1; // valor de error
+    }
+    else {
+        return a / b;
+    }
+}
+
+// Versión de la función que lanza una excepción
+int divideException(int a, int b) {
+    if (b == 0) {
+        throw std::invalid_argument("División por cero"); // lanza una excepción
+    }
+    else {
+        return a / b;
+    }
+}
---
+++ ej2/EJ2.h
@@ -0,0 +1,12 @@
+
+
+#ifndef EJ2_H
+#define EJ2_H
+
+
+int divideError(int a, int b);
+
+// Versión de la función que lanza una excepción
+int divideException(int a, int b);
+
+#endif //EJ2_H
