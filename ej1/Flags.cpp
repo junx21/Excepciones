d5d43fb4179f4b21377144a47e47770d8e734b23
junx21 <jxuche@myuax.com>
2025-01-06 15:37:35 +0100

    ej1

---
+++ ej1/Flags.cpp
@@ -0,0 +1,15 @@
+#include "Flags.h"
+
+// Variable global que actúa como bandera de error
+bool errorFlag = false;
+
+int divide(int a, int b) {
+    if (b == 0) {
+        // Si ocurre un error, se establece la bandera de error
+        errorFlag = true;
+        return 0;
+    }
+    else {
+        return a / b;
+    }
+}
---
+++ ej1/Flags.h
@@ -0,0 +1,16 @@
+//
+// Created by danie on 06/01/2025.
+//
+
+#ifndef FLAGS_H
+#define FLAGS_H
+
+
+extern bool errorFlag;
+
+// Función que divide dos enteros
+int divide(int a, int b);
+
+
+
+#endif //FLAGS_H
